c3a3e72e117139d13f2dcd12d01dfac520b4e743
kilco <jacques61593@gmail.com>
2022-05-31 20:21:22 +0200

    with image files

--- routes/stuff.js
+++ routes/stuff.js
@@ -7,12 +7,12 @@ const multer = require('../middleware/multer-config');
 const stuffCtrl = require('../controllers/stuff');
 const like = require('../controllers/like');
 
-router.get('/sauces', auth, stuffCtrl.findAllSauces);
-router.post('/sauces', auth, multer, stuffCtrl.createThing);
-router.get('/sauces/:id', auth, stuffCtrl.findOneSauce);
-router.put('/sauces/:id', auth, multer, stuffCtrl.modifyThing);
-router.delete('/sauces/:id', auth, stuffCtrl.deleteThing);
+router.get('/sauces', auth, stuffCtrl.findAllSauces);  // methode get pour lire
+router.post('/sauces', auth, multer, stuffCtrl.createThing);  // methode post permet de cree
+router.get('/sauces/:id', auth, stuffCtrl.findOneSauce); // permet de trouver et lire une sauce
+router.put('/sauces/:id', auth, multer, stuffCtrl.modifyThing); //permet de modifier la sauce
+router.delete('/sauces/:id', auth, stuffCtrl.deleteThing); //permet de supprimer une sauce
 
-router.post('/sauces/:id/like' , auth, like.likeDislike);
+router.post('/sauces/:id/like' , auth, like.likeDislike); // permet de cree un like
 
 module.exports = router;
